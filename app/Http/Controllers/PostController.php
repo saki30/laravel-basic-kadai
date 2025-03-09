1959be39837b8aa40063b74ed769c7ab7c058ad6
saki30 <s6u6s024@gmail.com>
2025-03-09 23:48:17 +0900

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller 
 {
-        public function index() {
-            return view('index');
-        }
+    public function index() {
+        $products = DB::table('products')->get();
+
+        return view('posts.index',compact('products'));
+    }   
 }
