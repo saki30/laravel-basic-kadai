16810c198568c2a04d44925fbf4edcf336795799
saki30 <s6u6s024@gmail.com>
2025-03-24 22:12:48 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,5 +21,10 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
