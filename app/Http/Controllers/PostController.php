8c59b1e9dd1a450aa8411ba80480d4c43b511f44
saki30 <s6u6s024@gmail.com>
2025-03-24 22:15:52 +0900

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,19 +4,43 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+use Illuminate\Http\store;
 
-class PostController extends Controller 
+class PostController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = DB::table('products')->get();
 
-        return view('posts.index',compact('products'));
-    }   
+        return view('posts.index', compact('products'));
+    }
 
-    public function show($id) {
-      
+    public function show($id)
+    {
+
         $products = Product::find($id);
 
         return view('posts.show', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200',
+        ]);
+
+        $vendor = new Product();
+        $vendor->title = $request->input('title');
+        $vendor->content = $request->input('content');
+        $vendor->save();
+
+        return redirect("/posts/create");
     }
 }
